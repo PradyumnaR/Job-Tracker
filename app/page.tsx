970d83d588338e221b4ac2146243ad698c5b898d
Pradyumna Ravuri <pradyumnaravuri@Pradyumnas-MBP.lan>
2024-09-18 12:19:20 -0700

    logo changes

--- app/page.tsx
+++ app/page.tsx
@@ -9,19 +9,25 @@ export default function Home() {
   return (
     <main>
       <header className='max-w-6xl mx-auto px-4 sm:px-8 py-6'>
-        <Image src={Logo} alt='main logo' />
+        <div className='flex flex-row items-center'>
+          <Image src={Logo} alt='logo' className=' mr-4 w-20 h-20' />
+          <h2 className='text-2xl'>Job-Tracker</h2>
+        </div>
       </header>
       <section className='max-w-6xl mx-auto px-4 sm:px-8 h-screen -mt-20 grid lg:grid-cols-[1fr,400px] items-center'>
-        <div>
-          <h1 className='capitalize text-4xl md:text-7xl font-bold'>
-            job <span className='text-primary'>tracking</span> app
-          </h1>
-          <p className='leading-loose max-w-md mt-4'></p>
+        <div className=''>
+          <p className='leading-loose max-w-md mt-4'>
+            Job tracker keeps track of applied jobs.
+          </p>
           <Button asChild className='mt-4'>
             <Link href='/add-job'>Get Started</Link>
           </Button>
         </div>
-        <Image src={LandingImg} alt='landing' className='hidden lg:block' />
+        <Image
+          src={LandingImg}
+          alt='landing'
+          className='w-96 h-96 hidden lg:block'
+        />
       </section>
     </main>
   );
--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -10,7 +10,10 @@ export default function Sidebar() {
   const pathname = usePathname();
   return (
     <aside className='py-4 px-8 bg-slate-300 h-full'>
-      <Image src={Logo} alt='logo' className='mx-auto' />
+      <div className='flex flex-row items-center'>
+        <Image src={Logo} alt='logo' className=' mr-4 w-20 h-20' />
+        <h2 className='text-2xl'>Job-Tracker</h2>
+      </div>
       <div className='flex flex-col mt-20 gap-y-4'>
         {NavLinks.map((link) => {
           return (
